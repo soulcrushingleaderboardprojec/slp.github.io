79457ef3115e9e200dcb405faa2554cdbe1911fc
soulcrushingleaderboardprojec <henriquepassis616@gmail.com>
2025-10-18 13:12:22 +0200

    Update Google Sheets API URL in get_data function

--- utils/funcs.py
+++ utils/funcs.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def get_data(r):
-    url = f"https://sheets.googleapis.com/v4/spreadsheets/1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc/values/{r}?key={os.getenv("GOOGLE_SHEETS_API_KEY")}"
+    url = f"https://sheets.googleapis.com/v4/spreadsheets/1_xs_YjbpIFl59T9FGhT-Y4aA5co_Bcm4R7OlLB1YkmY/values/{r}?key={os.getenv("GOOGLE_SHEETS_API_KEY")}"
     
     response = requests.get(url).json()
     values = response.get("values", [])
